6ed011d84fdbebf7b6f03b1a46116290228f6704
kuandry <cariocahdouglas@gmail.com>
2025-06-03 21:30:59 -0300

    Criação da section hero

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -6,8 +6,8 @@ import { Logo } from "../logo";
 export const Header = () => {
     return (
         <header className="fixed top-0 z-50 w-full border-b border-white/10 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
-            <div className="mx-auto max-w-12xl px-4 sm:px-6 lg:px-70">
-                <div className="flex h-20 items-center justify-around">
+            <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-80">
+                <div className="flex h-16 items-center justify-between">
                     <Logo />
 
                     <nav className="flex items-center gap-6">
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,9 +3,9 @@ import { HeroSection } from "@/components/hero-section";
 export default function Home() {
   return (
     <>
-    <article className="flex flex-col">
-      <HeroSection />
-    </article>
+      <article className="flex flex-col">
+        <HeroSection />
+      </article>
     </>
   );
 }
